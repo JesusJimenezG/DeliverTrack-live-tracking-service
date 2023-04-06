f0ddcbce6b8f6f4e1bc9d11a4955a7af71cb25e6
Jesus <jesusalfonsojg7@gmail.com>
2023-04-06 17:02:06 -0400

    Updated app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,13 +9,13 @@ export const createApp = () => {
     const app = express();
 
     // Set up Express app and HTTP server
-    app.use(cookieParser());
     app.use(
         cors({
             origin: 'http://localhost:5173',
             credentials: true
         })
     );
+    app.use(cookieParser());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
